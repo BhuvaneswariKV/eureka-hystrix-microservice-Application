2ade99bf2f62cee3dda9b69f4746f27b8aa9c71d
BhuvaneswariKV <bhuvaneswarikmca@gmail.com>
2016-11-23 21:56:56 +0530

    add new start_script.sh

---
+++ hystrix-dashboard/start_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update
+apt-get install curl -y
+while [ true ];
+do
+        curl --connect-timeout 20 localhost:8888 > /dev/null 2>&1
+        if [ "$?" == 0 ]; then
+                echo "Config server is available"
+                java -jar hystrix-dashboard-0.0.1-SNAPSHOT.jar > hystrix_service.log
+        fi
+        echo "Config server is not available; waiting for 10s"
+        sleep 10
+done
